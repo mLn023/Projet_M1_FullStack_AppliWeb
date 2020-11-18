3a3c05822f116378828c04613e39f63405f7b3d9
Mhilan <43454965+mLn023@users.noreply.github.com>
2020-11-18 17:57:25 +0100

    Initial commit

---
+++ src/main/java/fr/project/fullstack/ProjetBugFullStackApplication.java
@@ -0,0 +1,13 @@
+package fr.project.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetBugFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetBugFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/project/fullstack/ProjetBugFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.project.fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetBugFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
